0a72078f3891d4ce357f34e104672243c47744e1
SohailaN12 <sohaila.noori2027@gbwl.org>
2026-09-25 15:48:58 +0000

    notes week 2

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
 #def name_of_function():
 #indent for code
 #that belongs to function 
+#     return variable ---> this is optional, if we don't write return, it will automatically return None 
 
     number = "5.0"
     number = int(float(number))
@@ -23,6 +24,33 @@ def name_of_function():
 
 def function_with_args(name):
     print(f"Hello, thank you for your focus{name}")
+
+def calculate_area(width, height):
+    return width * height
+
+# we pass arguments to our function
+def greeting(name):
+    return f"Hello, {name}"
+
+def make_a_fraction(x,y=1):
+    return f"{x}/{y}"
+
+def put_under_one(y,x=1):
+    return f"{x}/{y}"
+
+def put_under_one_alt(x):
+    return f"1/{x}"
+
+def main():
+    # We can store return output into variables for later use
+    my_greeting = greeting("Ms. Dinko")
+    print(my_greeting)
+
+    # print(make_a_fraction(15,4))
+    print(make_a_fraction(15))
+    
+
+
 
 if __name__ == "__main__":
     main()
